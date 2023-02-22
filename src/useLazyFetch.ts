d373979f3a47509b8a65b6565bf135e1c4f65d8a
Chris <fuleinist@outlook.com>
2023-02-23 06:28:34 +1100

    Update useLazyFetch.ts

--- src/useLazyFetch.ts
+++ src/useLazyFetch.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react'
 
 export const useLazyFetch = (url: string, options: RequestInit) => {
   const controllerRef = useRef<AbortController>()
@@ -8,48 +8,51 @@ export const useLazyFetch = (url: string, options: RequestInit) => {
   const [loading, setLoading] = useState<boolean>(false)
   const [called, setCalled] = useState<boolean>(false)
 
-  const runFetch = async (data: any) => {
-    setCalled(true)
-    setLoading(true)
-    // Create an instance.
-    const controller = new AbortController()
-    controllerRef.current = controller
-    const signal = controller.signal
-    try {
-      let fetchUrl = url
-      let fetchOptions = {}
-      //default method is GET
-      const method = options?.method || 'GET'
-      if (method === 'GET' && data) {
-        fetchUrl = `${url}?${new URLSearchParams(data)}`
-        fetchOptions = {
-          ...options,
-          method,
-          signal,
+  const runFetch = useCallback(
+    async (data: any) => {
+      setCalled(true)
+      setLoading(true)
+      // Create an instance.
+      const controller = new AbortController()
+      controllerRef.current = controller
+      const signal = controller.signal
+      try {
+        let fetchUrl = url
+        let fetchOptions = {}
+        //default method is GET
+        const method = options?.method || 'GET'
+        if (method === 'GET' && data) {
+          fetchUrl = `${url}?${new URLSearchParams(data)}`
+          fetchOptions = {
+            ...options,
+            method,
+            signal,
+          }
+        } else {
+          fetchOptions = {
+            ...options,
+            method,
+            signal,
+            body: data,
+          }
         }
-      } else {
-        fetchOptions = {
-          ...options,
-          method,
-          signal,
-          body: data,
-        }
+        const response = await fetch(fetchUrl, fetchOptions)
+        const result = await response.json()
+        setResult(result)
+        setResponse(response)
+        setLoading(false)
+      } catch (e) {
+        setError(e)
+        setResponse(null)
+        setLoading(false)
       }
-      const response = await fetch(fetchUrl, fetchOptions)
-      const result = await response.json()
-      setResult(result)
-      setResponse(response)
-      setLoading(false)
-    } catch (e) {
-      setError(e)
-      setResponse(null)
-      setLoading(false)
-    }
-  }
+    },
+    [url]
+  )
 
   useEffect(() => {
+    controllerRef?.current?.abort()
     return () => {
-      controllerRef?.current?.abort()
       setCalled(false)
     }
   }, [controllerRef])
